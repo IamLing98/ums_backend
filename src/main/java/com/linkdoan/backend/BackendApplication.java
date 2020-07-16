b7fe6bf1107c88dacae0766dd76df7abee76a5ad
LingDev <linhdeptrai1029i@gmail.com>
2020-07-17 04:31:14 +0700

    a

--- src/main/java/com/linkdoan/backend/BackendApplication.java
+++ src/main/java/com/linkdoan/backend/BackendApplication.java
@@ -4,12 +4,13 @@ import com.linkdoan.backend.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
         FileStorageProperties.class
 })
-public class BackendApplication {
+public class BackendApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
