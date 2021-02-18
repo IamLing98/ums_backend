2aace1be2f5c01a519190be8ba16cc9e5ba6d4f9
lingdev1998 <linhdeptrai1029i@gmail.com>
2021-02-18 23:51:50 +0700

    feature/crm

--- src/main/java/com/linkdoan/backend/base/StompPrincipal.java
+++ src/main/java/com/linkdoan/backend/base/StompPrincipal.java
@@ -2,9 +2,6 @@ package com.linkdoan.backend.base;
 
 import java.security.Principal;
 
-/**
- * @author - Evgeniy Grechishnikov
- */
 public class StompPrincipal implements Principal {
 
     private String name;
